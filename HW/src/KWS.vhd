0060dec6465dd2fc9eb9b3d034376ea5e16aff3b
Vic0303 <wmatykiewicz@student.agh.edu.pl>
2025-12-23 13:49:47 +0100

    HW: modify KWS architecture for testing

--- HW/src/KWS.vhd
+++ HW/src/KWS.vhd
@@ -8,8 +8,8 @@ entity KWS is
         F_WIDTH     : INTEGER := 7;
         THROUGHPUT  : INTEGER := 400;
         NUM_CHANNEL : integer := 128;
-        WEIGHT      : integer := 1;
-        DECAY_SHIFT : integer := 4;
+        WEIGHT      : integer := 32;
+        DECAY_SHIFT : integer := 8;
         PRECISION_GEN  : integer := 8 
     );
     Port (
@@ -17,16 +17,20 @@ entity KWS is
         clock_48     : in std_logic;
         rst_ext      : in std_logic;
 
-        -- Input Interface (from timestamp_gen)
+        -- Input Interface
         in_t         : in STD_LOGIC_VECTOR(T_WIDTH-1  downto 0);
         in_f         : in STD_LOGIC_VECTOR(F_WIDTH-1 downto 0);
         in_valid     : in STD_LOGIC;
-        out_busy     : out STD_LOGIC; 
 
-        -- GCNN Classification Output
+        -- GCNN Output
         cnn_valid    : out STD_LOGIC;
         cnn_conf     : out STD_LOGIC_VECTOR(PRECISION_GEN-1 downto 0);
         cnn_class    : out STD_LOGIC_VECTOR((8*20)-1 downto 0)
+
+
+--         debug_valid  : out std_logic;
+--         debug_t      : out std_logic_vector(T_WIDTH-1 downto 0);
+--         debug_f      : out std_logic_vector(F_WIDTH-1 downto 0)
     );
 end KWS;
 
@@ -47,7 +51,6 @@ architecture Behavioral of KWS is
             in_req    : in  std_logic;
             in_t      : in  std_logic_vector(T_WIDTH-1 downto 0);
             in_f      : in  std_logic_vector(F_WIDTH-1 downto 0);
-            busy      : out std_logic;
             out_valid : out std_logic;
             out_t     : out std_logic_vector(T_WIDTH-1 downto 0);
             out_f     : out std_logic_vector(F_WIDTH-1 downto 0)
@@ -79,20 +82,16 @@ architecture Behavioral of KWS is
             t         : in std_logic_vector(T_WIDTH-1 downto 0);
             f         : in std_logic_vector(F_WIDTH-1 downto 0);
             is_valid  : in std_logic;
-
             out_valid : out std_logic;
             out_conf  : out std_logic_vector(PRECISION_GEN-1 downto 0);
             out_cls   : out std_logic_vector((8*20)-1 downto 0)
         );
     END COMPONENT;
 
-    -- LIF signals
-    signal lif_busy       : std_logic;
     signal lif_out_valid  : std_logic;
     signal lif_out_t      : std_logic_vector(T_WIDTH-1 downto 0);
     signal lif_out_f      : std_logic_vector(F_WIDTH-1 downto 0);
 
-    -- FIFO signals
     signal fifo_din       : std_logic_vector(39 downto 0);
     signal fifo_wr_en     : std_logic;
     signal dout           : std_logic_vector(39 downto 0);
@@ -102,7 +101,6 @@ architecture Behavioral of KWS is
     signal rd_rst_busy    : std_logic;
     signal rd_en          : std_logic := '0';
 
-    -- Output of FIFO -> Input of GCNN
     signal valid_r        : std_logic := '0';
     signal t_r            : std_logic_vector(T_WIDTH-1 downto 0);
     signal f_r            : std_logic_vector(F_WIDTH-1 downto 0);
@@ -111,11 +109,6 @@ architecture Behavioral of KWS is
 
 begin
 
-    out_busy <= lif_busy or wr_rst_busy;
-
-    -------------------------------------------------------------------------
-    -- LIF FILTER INSTANCE (Clock Domain: 48 MHz)
-    -------------------------------------------------------------------------
     lif_inst : lif
     GENERIC MAP (
         T_WIDTH     => T_WIDTH,
@@ -130,20 +123,16 @@ begin
         in_req    => in_valid,
         in_t      => in_t,
         in_f      => in_f,
-        busy      => lif_busy,
         out_valid => lif_out_valid,
         out_t     => lif_out_t,
         out_f     => lif_out_f
     );
 
-    -------------------------------------------------------------------------
-    -- FIFO INSTANCE (CDC: 48 MHz -> 200 MHz)
-    -------------------------------------------------------------------------
     fifo_wr_en <= lif_out_valid; 
-    fifo_din   <= lif_out_t & lif_out_f & '1'; 
+    fifo_din   <= lif_out_t & lif_out_f & '0'; 
 
     FIFO_CDC : fifo_generator_1
-      PORT MAP (
+    PORT MAP (
         srst        => rst_ext,
         wr_clk      => clock_48,
         rd_clk      => clock_200,
@@ -155,11 +144,8 @@ begin
         empty       => empty,
         wr_rst_busy => wr_rst_busy,
         rd_rst_busy => rd_rst_busy
-      );
+    );
 
-    -------------------------------------------------------------------------
-    -- READ SIDE LOGIC (Clock Domain: 200 MHz)
-    -------------------------------------------------------------------------
     rd: process(clock_200, rst_ext)
     begin
         if rst_ext = '1' then
@@ -167,7 +153,6 @@ begin
             read_timer <= 0;
         elsif rising_edge(clock_200) then
             rd_en <= '0';
-
             if read_timer < THROUGHPUT then
                 read_timer <= read_timer + 1;
             end if;
@@ -180,25 +165,31 @@ begin
             end if;
         end if;
     end process;
-
-    unpack_to_GCNN_input: process(clock_200, rst_ext)
+    
+    unpack: process(clock_200, rst_ext)
+        variable rd_en_d1 : std_logic := '0';
     begin
         if rst_ext = '1' then
             valid_r <= '0';
             t_r     <= (others => '0');
             f_r     <= (others => '0');
+            rd_en_d1 := '0';
         elsif rising_edge(clock_200) then
-            if rd_en = '1' then
-                valid_r <= dout(0);
-                f_r     <= dout(F_WIDTH downto 1);
-                t_r     <= dout(39 downto F_WIDTH+1);
-            else
-                valid_r <= '0';
+            valid_r <= rd_en_d1;
+            
+            if rd_en_d1 = '1' then
+                f_r <= dout(F_WIDTH downto 1);
+                t_r <= dout(39 downto F_WIDTH+1);
             end if;
+            
+            rd_en_d1 := rd_en;
         end if;
     end process;
 
-    
+--     debug_valid <= valid_r;
+--     debug_t     <= t_r;
+--     debug_f     <= f_r;
+
     gcnn_inst : gcnn_top
     PORT MAP (
         clk       => clock_200,
@@ -206,7 +197,6 @@ begin
         t         => t_r,
         f         => f_r,
         is_valid  => valid_r,
-        
         out_valid => cnn_valid,
         out_conf  => cnn_conf,
         out_cls   => cnn_class
