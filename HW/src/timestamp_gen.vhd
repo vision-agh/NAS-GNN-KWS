2cd41867ba9186be41c7897df09080cd38dccd29
Vic0303 <wmatykiewicz@student.agh.edu.pl>
2025-12-29 03:21:13 +0100

    HW: timestamp gen update

--- HW/src/timestamp_gen.vhd
+++ HW/src/timestamp_gen.vhd
@@ -18,7 +18,6 @@ entity timestamp_gen is
         out_t     : out STD_LOGIC_VECTOR(T_WIDTH-1 downto 0);
         out_f     : out STD_LOGIC_VECTOR(F_WIDTH-1 downto 0);
         out_valid : out STD_LOGIC
-
     );
 end timestamp_gen;
 
@@ -34,17 +33,14 @@ architecture Behavioral of timestamp_gen is
     signal req_prev       : std_logic := '0';
     signal req_rising     : std_logic;
     
-    signal ack_reg        : std_logic := '0';
-
 begin
-
 
     ts: process(clk, rst)
     begin
         if rst = '1' then
             ts_div_count <= (others => '0');
             t_counter    <= (others => '0');
-            timer_active <= '0';   
+            timer_active <= '0';    
         elsif rising_edge(clk) then
             if timer_active = '0' and req_synced = '1' then
                 timer_active <= '1';
@@ -69,25 +65,22 @@ begin
         if rst = '1' then
             req_sync_reg <= "00";
             req_prev     <= '0';
-            ack_reg      <= '0';
             out_valid    <= '0';
             out_t        <= (others => '0');
             out_f        <= (others => '0');
         elsif rising_edge(clk) then
             req_sync_reg <= req_sync_reg(0) & AER_REQ;
             req_prev     <= req_synced;
-            out_valid <= '0';
+            out_valid    <= '0';
             
             if req_rising = '1' then
                 out_t     <= std_logic_vector(t_counter);
                 out_f     <= AER_DATA; 
                 out_valid <= '1';
             end if;
-            ack_reg <= req_synced;
-
         end if;
     end process;
 
-    AER_ACK <= ack_reg;
+    AER_ACK <= req_synced;
 
 end Behavioral;
