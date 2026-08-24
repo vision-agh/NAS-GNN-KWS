39812bb94e4e1f1a21a200993411974f8329862e
Wiktor Matykiewicz <wmatykiewicz@student.agh.edu.pl>
2026-08-24 23:05:20 +0200

    SW: fix typo in run script

--- HW/tcl/run_kws_sys.tcl
+++ HW/tcl/run_kws_sys.tcl
@@ -18,4 +18,4 @@ puts "Running implementation and bitstream generation..."
 launch_runs impl_1 -to_step write_bitstream -jobs 10
 wait_on_run impl_1
 
-puts "NAS KWS synthesis, implementation, and bitstream generation completed successfully!"W
+puts "NAS KWS synthesis, implementation, and bitstream generation completed successfully!"
